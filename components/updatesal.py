7923171943780ffc79baf5947f0246d8046688e4
Amit Yadav <amitkumar8107474@gmail.com>
2023-08-17 11:59:53 +0530

    first commit

--- components/updatesal.py
+++ components/updatesal.py
@@ -3,6 +3,5 @@ import customtkinter
 class UpdateSalary(customtkinter.CTkScrollableFrame):
     def __init__(self,master):
         super().__init__(master, label_text='Update Salary')
-
         self.grid(row=0, column=1,rowspan=4, padx=(20, 0), pady=(20,0), sticky="nsew")
         
